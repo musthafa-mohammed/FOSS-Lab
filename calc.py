a25b95fd559294020a749786894bfc3076b34704
MOHAMMED MUSTHAFA K <musthafamumpang@gmail.com>
2017-04-17 11:59:25 +0530

    ui added

---
+++ calc.py
@@ -0,0 +1,51 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class GridWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Calculator")
+
+        grid = Gtk.Grid()
+        self.add(grid)
+        entry=Gtk.Entry()
+        button1 = Gtk.Button(label="1")
+        button2 = Gtk.Button(label="2")
+        button3 = Gtk.Button(label="3")
+        button4 = Gtk.Button(label="4")
+        button5 = Gtk.Button(label="5")
+        button6 = Gtk.Button(label="6")
+        button7 = Gtk.Button(label="7")
+        button8 = Gtk.Button(label="8")
+        button9 = Gtk.Button(label="9")
+        button0 = Gtk.Button(label="0")
+        buttonp = Gtk.Button(label="+")
+        buttonmi = Gtk.Button(label="-")
+        buttonmu = Gtk.Button(label="*")
+        buttond = Gtk.Button(label="/")
+        buttonc = Gtk.Button(label="c")
+        buttone = Gtk.Button(label="=")
+
+ 
+        grid.attach(entry,0,0,3,1)
+        grid.attach(button1, 0, 1, 1, 1)
+        grid.attach(button2, 1, 1, 1, 1)
+        grid.attach(button3, 2, 1, 1, 1)
+        grid.attach(button4, 0, 2, 1, 1)
+        grid.attach(button5, 1, 2, 1, 1)
+        grid.attach(button6, 2, 2, 1, 1)
+        grid.attach(button7, 0, 3, 1, 1)
+        grid.attach(button8, 1, 3, 1, 1)
+        grid.attach(button9, 2, 3, 1, 1)
+        grid.attach(button0, 0, 4, 1, 1)
+        grid.attach(buttone, 1, 4, 1, 1)
+        grid.attach(buttonc, 2, 4, 1, 1)
+        grid.attach(buttonp, 3, 1, 1, 1)
+        grid.attach(buttonmi, 3, 2, 1, 1)
+        grid.attach(buttonmu, 3, 3, 1, 1)
+        grid.attach(buttond, 3, 4, 1, 1)
+win = GridWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
