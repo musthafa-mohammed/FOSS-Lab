383e1eddc54cf9adb353985a7f8256d2612a760d
MOHAMMED MUSTHAFA K <musthafamumpang@gmail.com>
2017-04-18 21:48:21 +0530

    compleated

--- calc.py
+++ calc.py
@@ -1,15 +1,21 @@
 import gi
+import time
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class GridWindow(Gtk.Window):
+
 
     def __init__(self):
         Gtk.Window.__init__(self, title="Calculator")
 
         grid = Gtk.Grid()
         self.add(grid)
-        entry=Gtk.Entry()
+        self.entry=Gtk.Entry()
+        self.st=''
+        self.ls=[]
+        self.flag=''
+
         button1 = Gtk.Button(label="1")
         button2 = Gtk.Button(label="2")
         button3 = Gtk.Button(label="3")
@@ -26,9 +32,26 @@ class GridWindow(Gtk.Window):
         buttond = Gtk.Button(label="/")
         buttonc = Gtk.Button(label="c")
         buttone = Gtk.Button(label="=")
+        button1.connect("clicked",self.on_click_button)
+        button2.connect("clicked",self.on_click_button)
+        button3.connect("clicked",self.on_click_button)
+        button4.connect("clicked",self.on_click_button)
+        button5.connect("clicked",self.on_click_button)
+        button6.connect("clicked",self.on_click_button)
+        button7.connect("clicked",self.on_click_button)
+        button8.connect("clicked",self.on_click_button)
+        button9.connect("clicked",self.on_click_button)
+        button0.connect("clicked",self.on_click_button)
+        buttonp.connect("clicked",self.on_click_button)
+        buttonmi.connect("clicked",self.on_click_button)
+        buttonmu.connect("clicked",self.on_click_button)
+        buttond.connect("clicked",self.on_click_button)
+        buttonc.connect("clicked",self.on_click_button)
+        buttone.connect("clicked",self.on_click_button)
+
 
  
-        grid.attach(entry,0,0,3,1)
+        grid.attach(self.entry,0,0,3,1)
         grid.attach(button1, 0, 1, 1, 1)
         grid.attach(button2, 1, 1, 1, 1)
         grid.attach(button3, 2, 1, 1, 1)
@@ -45,6 +68,63 @@ class GridWindow(Gtk.Window):
         grid.attach(buttonmi, 3, 2, 1, 1)
         grid.attach(buttonmu, 3, 3, 1, 1)
         grid.attach(buttond, 3, 4, 1, 1)
+    def on_click_button(self,button):
+        if button.props.label=="c":
+            self.st = self.st[:-1]
+            self.entry.set_text(self.st)
+        elif button.props.label=="=":
+            if len(self.ls)!=1:
+                self.entry.set_text("ooops")
+                #self.entry.set_text("")
+                print self.ls
+                self.st=''
+                self.ls=[]
+            else:
+                #print self.ls
+                self.ls.append(int(self.st))
+                if self.flag=="+":
+                 self.entry.set_text(str(self.ls[0]+self.ls[1]))
+                elif self.flag=="-":
+                 self.entry.set_text(str(self.ls[0]-self.ls[1]))
+                elif self.flag=="*":
+                 self.entry.set_text(str(self.ls[0]*self.ls[1]))
+                if self.flag=="/":
+                 self.entry.set_text(str(self.ls[0]/self.ls[1]))
+
+                self.ls=[]
+                self.st=''
+        elif button.props.label=="1" or button.props.label=="2" or button.props.label=="3" or button.props.label=="4" or button.props.label=="5" or button.props.label=="6" or  button.props.label=="7" or  button.props.label=="8" or button.props.label=="9" or  button.props.label=="0":
+            self.st=self.st+button.props.label
+            self.entry.set_text(self.st)
+        elif button.props.label=="+":
+            self.flag="+"
+            self.ls.append(int(self.st))
+            self.entry.set_text("")
+            self.st=''
+
+        elif button.props.label=="-":
+            self.flag="-"
+            self.ls.append(int(self.st))
+            self.entry.set_text("")
+            self.st=''
+    
+
+        elif button.props.label=="*":
+            self.flag="*"
+            self.ls.append(int(self.st))
+            self.entry.set_text("")
+            self.st=''
+
+        else:
+            self.flag="/"
+            self.ls.append(int(self.st))
+            self.entry.set_text("")
+            self.st=''
+
+            
+            
+            
+        
 win = GridWindow()
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
