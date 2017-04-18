1ca018fa5ee23181071e13a8a17db28cfb9e5705
MOHAMMED MUSTHAFA K <musthafamumpang@gmail.com>
2017-04-18 22:00:40 +0530

    Create mariadb.pl

---
+++ mariadb.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use DBI;
+use strict;
+my $driver = "mysql"; 
+my $database = "student";
+my $dsn = "DBI:$driver:database=$database";
+my $userid = "root";						#Username often root
+my $password = "";						#your Password
+my $dbh = DBI->connect($dsn, $userid, $password ) or die $DBI::errstr;
+my $sth = $dbh->prepare("INSERT INTO student
+                       (name , phone )
+                        values
+                       ('poke',111);");
+$sth->execute() or die $DBI::errstr;
+$sth->finish();
